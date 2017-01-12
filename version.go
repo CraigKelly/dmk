ad523310d1cdba087a14631de968794db3c0881e
Craig Kelly <craig.n.kelly@gmail.com>
2017-01-12 15:47:45 -0600

    whoops - always include version.go in scc

---
+++ version.go
@@ -0,0 +1,7 @@
+package main
+
+// Version returns the current version string for this package - it is
+// generated from the file VERSION in the root of the repository
+func Version() string {
+    return "0.0.1 (built Thu Jan 12 15:47:27 2017)"
+}
