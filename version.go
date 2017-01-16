5e32ae499b07613bc4dd4a7519888230ed50e080
Craig Kelly <craig.n.kelly@gmail.com>
2017-01-15 21:28:11 -0600

    version 0.0.3

--- version.go
+++ version.go
@@ -3,5 +3,5 @@ package main
 // Version returns the current version string for this package - it is
 // generated from the file VERSION in the root of the repository
 func Version() string {
-    return "0.0.2 (built Sun Jan 15 21:16:55 2017)"
+    return "0.0.3 (built Sun Jan 15 21:27:52 2017)"
 }
