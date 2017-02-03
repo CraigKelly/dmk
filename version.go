e0bc84b81bf5db66b632aae587b68bbc41b6b488
Craig Kelly <craig.n.kelly@gmail.com>
2017-02-03 10:47:08 -0600

    Update dependencies

--- version.go
+++ version.go
@@ -3,5 +3,5 @@ package main
 // Version returns the current version string for this package - it is
 // generated from the file VERSION in the root of the repository
 func Version() string {
-	return "0.0.4 (built Wed Jan 18 12:03:22 2017)"
+    return "0.0.4 (built Fri Feb  3 10:45:03 2017)"
 }
