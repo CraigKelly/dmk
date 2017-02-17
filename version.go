a73fa4ac03d21fe2e08e5ef19a68834e8f4b4d96
Craig Kelly <craig.n.kelly@gmail.com>
2017-02-17 15:27:31 -0600

    Looks like we're good for 0.1!

--- version.go
+++ version.go
@@ -3,5 +3,5 @@ package main
 // Version returns the current version string for this package - it is
 // generated from the file VERSION in the root of the repository
 func Version() string {
-    return "0.0.4 (built Wed Feb  8 14:56:29 2017)"
+	return "0.1.0 (built Fri Feb 17 15:27:25 2017)"
 }
