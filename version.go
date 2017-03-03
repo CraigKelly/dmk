34bbde92304cc16e51d927b8f6b35f47a952298a
Craig Kelly <craig.n.kelly@gmail.com>
2017-03-03 09:58:55 -0600

    Updating to go 1.8 and using latest godep

--- version.go
+++ version.go
@@ -3,5 +3,5 @@ package main
 // Version returns the current version string for this package - it is
 // generated from the file VERSION in the root of the repository
 func Version() string {
-    return "0.1.0 (built Fri Feb 17 15:29:26 2017)"
+    return "0.1.0 (built Fri Mar  3 09:57:39 2017)"
 }
