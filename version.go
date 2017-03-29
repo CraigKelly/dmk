95feb01c722e22a0ea9f3b8c5951374b61c62973
221019831 <Craig.Kelly@ge.com>
2017-03-29 15:51:50 +0000

    Better/safer update script

--- version.go
+++ version.go
@@ -3,5 +3,5 @@ package main
 // Version returns the current version string for this package - it is
 // generated from the file VERSION in the root of the repository
 func Version() string {
-	return "0.1.1 (built Fri Mar  3 10:00:07 2017)"
+    return "0.1.1 (built Wed Mar 29 15:50:52 2017)"
 }
