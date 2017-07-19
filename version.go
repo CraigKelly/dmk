9a8697f1c7d22482d92348f3d7163a87dca52dfe
Craig Kelly <craig.n.kelly@gmail.com>
2017-07-18 22:04:23 -0500

    Update vendored deps

--- version.go
+++ version.go
@@ -3,5 +3,5 @@ package main
 // Version returns the current version string for this package - it is
 // generated from the file VERSION in the root of the repository
 func Version() string {
-	return "0.1.2d (built Fri Jun 30 21:43:56 2017)"
+	return "0.1.2d (built Tue Jul 18 22:03:42 2017)"
 }
