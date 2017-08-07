0678a6b5d9ad79e170d4c0ee19584892374d0145
Craig Kelly <craig.n.kelly@gmail.com>
2017-08-07 12:39:32 -0500

    Fixed file completion

--- main.go
+++ main.go
@@ -12,8 +12,6 @@ import (
 /////////////////////////////////////////////////////////////////////////////
 // Entry point
 
-// TODO: update bash completion to show files/paths if preceeding flag is -f
-
 func main() {
 	log.SetFlags(0)
 	pcheck(os.Setenv("DMK_VERSION", Version()))
