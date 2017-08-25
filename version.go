8fb7429165e69f7b8745f2d0701a2e61534e6011
221019831 <Craig.Kelly@ge.com>
2017-08-25 16:24:20 +0000

    Update deps and move to go 1.9

--- version.go
+++ version.go
@@ -3,5 +3,5 @@ package main
 // Version returns the current version string for this package - it is
 // generated from the file VERSION in the root of the repository
 func Version() string {
-	return "0.1.2d (built Mon Aug 14 21:04:28 2017)"
+	return "0.1.2d (built Fri Aug 25 16:23:55 2017)"
 }
