02cf73712f66f11ff37f2c20520630361104bdda
221019831 <Craig.Kelly@ge.com>
2017-10-04 17:48:16 +0000

    Bunmping version with new dep update

--- version.go
+++ version.go
@@ -3,5 +3,5 @@ package main
 // Version returns the current version string for this package - it is
 // generated from the file VERSION in the root of the repository
 func Version() string {
-	return "0.1.2d (built Thu Sep 14 15:17:20 2017)"
+	return "0.1.2e (built Wed Oct  4 17:48:03 2017)"
 }
