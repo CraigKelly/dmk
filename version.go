1320255f5dffb8e3eb0fa526864f1ffb2dcf7aaa
221019831 <Craig.Kelly@ge.com>
2017-12-05 16:43:59 +0000

    updating deps

--- version.go
+++ version.go
@@ -3,5 +3,5 @@ package main
 // Version returns the current version string for this package - it is
 // generated from the file VERSION in the root of the repository
 func Version() string {
-	return "0.1.2e (built Tue Dec  5 16:41:58 2017)"
+	return "0.1.2e (built Tue Dec  5 16:43:27 2017)"
 }
