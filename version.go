d6370963a08997b221c2a62ab56e84ed795b743f
221019831 <Craig.Kelly@ge.com>
2018-03-23 16:48:14 +0000

    release 1.0.0

--- version.go
+++ version.go
@@ -3,5 +3,5 @@ package main
 // Version returns the current version string for this package - it is
 // generated from the file VERSION in the root of the repository
 func Version() string {
-	return "0.1.3 (built Fri Mar 23 16:37:02 2018)"
+	return "1.0.0 (built Fri Mar 23 16:47:47 2018)"
 }
