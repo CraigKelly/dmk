1f2f0036e04bce5984a0e1b07237c029452803f1
Craig Kelly <craig.n.kelly@gmail.com>
2019-07-29 11:22:38 -0500

    dep updates

--- version.go
+++ version.go
@@ -3,5 +3,5 @@ package main
 // Version returns the current version string for this package - it is
 // generated from the file VERSION in the root of the repository
 func Version() string {
-	return "1.0.1 (built Mon Apr 30 17:20:45 2018)"
+	return "1.0.1 (built Mon Jul 29 11:22:16 2019)"
 }
